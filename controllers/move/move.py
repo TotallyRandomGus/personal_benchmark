9b18c5a733fe502117683047001430cb7f249cfe
Léo Duggan <leo.duggan@alumni.epfl.ch>
2022-10-17 11:07:19 +0200

    Copied benchmark code from Thomas

--- controllers/move/move.py
+++ controllers/move/move.py
@@ -4,14 +4,13 @@ from controller import Robot
 import sys
 
 # Define the target motor position in radians.
-target = 12
+target = 0
 
 # Get pointer to the robot.
 robot = Robot()
 
 # Print the program output on the console
 print("Move the motors of the Thymio II to position " + str(target) + ".")
-sys.stderr.write("This is a sample error message.\n")
 
 # Set the target position of the left and right wheels motors.
 robot.getDevice("motor.left").setPosition(target)
--- controllers/supervisor/supervisor.py
+++ controllers/supervisor/supervisor.py
@@ -4,20 +4,32 @@ from controller import Supervisor
 import os
 import sys
 
-try:
-    includePath = os.environ.get("WEBOTS_HOME") + "/projects/samples/robotbenchmark/include"
+def benchmarkPerformance(message, robot):
+    benchmark_name = message.split(':')[1]
+    benchmark_performance_string = message.split(':')[3]
+    print(benchmark_name + ' Benchmark complete! Your performance was ' + benchmark_performance_string)
+    if robot.getFromDef("ANIMATION_RECORDER_SUPERVISOR"):
+        stop_recording(robot, message)
+
+def stop_recording(robot, message):
+    emitter = robot.getDevice('emitter')
+    emitter.send(message.encode('utf-8'))
+
+""" try:
+    includePath = "../../../include"
     includePath.replace('/', os.sep)
     sys.path.append(includePath)
-    from robotbenchmark import robotbenchmarkRecord
+    from benchmark import benchmarkPerformance
 except ImportError:
-    sys.stderr.write("Warning: 'robotbenchmark' module not found.\n")
-    sys.exit(0)
+    print("error")
+    sys.stderr.write("Warning: 'benchmark' module not found.\n")
+    sys.exit(0) """
 
 robot = Supervisor()
 
 timestep = int(robot.getBasicTimeStep())
 
-thymio = robot.getFromDef("THYMIO2")
+thymio = robot.getFromDef("BENCHMARK_ROBOT")
 translation = thymio.getField("translation")
 
 tx = 0
@@ -29,8 +41,13 @@ while robot.step(timestep) != -1:
         if percent < 0:
             percent = 0
         if t[0] < -0.01 and abs(t[0] - tx) < 0.0001:  # away from starting position and not moving any more
-            message = "stop"
             running = False
+            name = 'Robot Programming'
+            performance = str(percent)
+            performanceString = str(round(percent * 100, 2)) + '%'
+            message = 'success:' + name + ':' + performance + ':' + performanceString
+            robot.wwiSendText(message)
+            benchmarkPerformance(message, robot)
         else:
             message = "percent"
         message += ":" + str(percent)
@@ -39,12 +56,11 @@ while robot.step(timestep) != -1:
     else:  # wait for record message
         message = robot.wwiReceiveText()
         while message:
-            if message.startswith("record:"):
-                record = robotbenchmarkRecord(message, "robot_programming", percent)
-                robot.wwiSendText(record)
-                break
-            elif message == "exit":
-                break
+            if message.startswith("confirm:"):
+                print("WINDOW MESSAGE:", message)
+            """ if message.startswith("success:"):
+                benchmarkPerformance(message, robot)
+                break """
             message = robot.wwiReceiveText()
 
 robot.simulationSetMode(Supervisor.SIMULATION_MODE_PAUSE)
