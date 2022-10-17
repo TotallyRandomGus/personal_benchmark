862c878200beca2a00d479dd917c396a0cf26391
Léo Duggan <leo.duggan@alumni.epfl.ch>
2022-10-17 10:18:20 +0200

    Updated description files

--- controllers/supervisor/supervisor.py
+++ controllers/supervisor/supervisor.py
@@ -1,101 +1,50 @@
-"""Controller program to manage the benchmark.
-
-It manages the perturbation and evaluates the peformance of the user
-controller.
-"""
+"""Supervisor of the Robot Programming benchmark."""
 
 from controller import Supervisor
-import math
 import os
-import random
 import sys
 
-def benchmarkPerformance(message, robot):
-    benchmark_name = message.split(':')[1]
-    benchmark_performance_string = message.split(':')[3]
-    print(benchmark_name + ' Benchmark complete! Your performance was ' + benchmark_performance_string)
-    if robot.getFromDef("ANIMATION_RECORDER_SUPERVISOR"):
-        stop_recording(robot, message)
-
-def stop_recording(robot, message):
-    emitter = robot.getDevice('emitter')
-    emitter.send(message.encode('utf-8'))
-
-def time_convert(time):
-    minutes = time / 60
-    absolute_minutes =  math.floor(minutes)
-    minutes_string = str(absolute_minutes).zfill(2)
-    seconds = (minutes - absolute_minutes) * 60
-    absolute_seconds =  math.floor(seconds)
-    seconds_string = str(absolute_seconds).zfill(2)
-    cs = math.floor((seconds - absolute_seconds) * 100);
-    cs_string = str(cs).zfill(2)
-    return minutes_string + "." + seconds_string + "." + cs_string
-
-""" try:
+try:
     includePath = os.environ.get("WEBOTS_HOME") + "/projects/samples/robotbenchmark/include"
     includePath.replace('/', os.sep)
     sys.path.append(includePath)
     from robotbenchmark import robotbenchmarkRecord
 except ImportError:
     sys.stderr.write("Warning: 'robotbenchmark' module not found.\n")
-    sys.exit(0) """
-
-# Get random generator seed value from 'controllerArgs' field
-seed = 1
-if len(sys.argv) > 1 and sys.argv[1].startswith('seed='):
-    seed = int(sys.argv[1].split('=')[1])
+    sys.exit(0)
 
 robot = Supervisor()
 
 timestep = int(robot.getBasicTimeStep())
 
-jointParameters = robot.getFromDef("PENDULUM_PARAMETERS")
-positionField = jointParameters.getField("position")
+thymio = robot.getFromDef("THYMIO2")
+translation = thymio.getField("translation")
 
-emitter = robot.getDevice("emitter")
-time = 0
-force = 0
-forceStep = 800
-random.seed(seed)
-run = True
-
+tx = 0
+running = True
 while robot.step(timestep) != -1:
-    if run:
-        time = robot.getTime()
-        robot.wwiSendText("time:%-24.3f" % time)
-        robot.wwiSendText("force:%.2f" % force)
-
-        # Detect status of inverted pendulum
-        position = positionField.getSFFloat()
-        if position < -1.58 or position > 1.58:
-            # stop
-            run = False
-            name = "Inverted Pendulum"
-            performance = str(time)
-            performanceString = time_convert(time)
-            message = 'success:' + name + ':' + performance + ':' + performanceString
-            robot.wwiSendText(message)
-            benchmarkPerformance(message, robot)
+    t = translation.getSFVec3f()
+    if running:
+        percent = 1 - abs(0.25 + t[0]) / 0.25
+        if percent < 0:
+            percent = 0
+        if t[0] < -0.01 and abs(t[0] - tx) < 0.0001:  # away from starting position and not moving any more
+            message = "stop"
+            running = False
         else:
-            if forceStep <= 0:
-                forceStep = 800 + random.randint(0, 400)
-                force = force + 0.02
-                toSend = "%.2lf %d" % (force, seed)
-                if sys.version_info.major > 2:
-                    toSend = bytes(toSend, "utf-8")
-                emitter.send(toSend)
-            else:
-                forceStep = forceStep - 1
-    else:
-        # wait for record message
+            message = "percent"
+        message += ":" + str(percent)
+        robot.wwiSendText(message)
+        tx = t[0]
+    else:  # wait for record message
         message = robot.wwiReceiveText()
         while message:
-            if message.startswith("confirm:"):
-                print("WINDOW MESSAGE:", message)
-            """ if message.startswith("success:"):
-                benchmarkPerformance(message, robot)
-                break """
+            if message.startswith("record:"):
+                record = robotbenchmarkRecord(message, "robot_programming", percent)
+                robot.wwiSendText(record)
+                break
+            elif message == "exit":
+                break
             message = robot.wwiReceiveText()
 
 robot.simulationSetMode(Supervisor.SIMULATION_MODE_PAUSE)
